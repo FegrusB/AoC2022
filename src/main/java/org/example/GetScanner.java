62dae0666805b0fdab8d14cce44760ddc2223639
FegrusB <fergus@beckerleg.co.uk>
2023-06-27 09:23:01 +0100

    Solve for day one

--- src/main/java/org/example/GetScanner.java
+++ src/main/java/org/example/GetScanner.java
@@ -1,32 +1,17 @@
-package aoc2021;
+package org.example;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
-//class with single method, to start scanners for reading in puzzle inputs. Finds path to current dir, returns scanner of txt file from data folder 
+
 class GetScanner {
-	
-	//
+
 	public static Scanner get(String fileName) {
-		
-		//strings to build address with, data folder, and absolute path to aoc2021
-		String data = "\\data\\";
-		String mainAdd = new File("").getAbsolutePath();
-		
-		//create file object with correct address and initialise myScanner
-		File myFile = new File(mainAdd + data + fileName + ".txt");
-		Scanner myScanner = null;
-		
-		//try to create scanner of file at myFile, throw exception if no file
-		try {
-			myScanner = new Scanner(myFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		//return final scanner
-		return myScanner;
-	
+
+		InputStream stream = GetScanner.class.getClassLoader().getResourceAsStream("PuzzleText\\" + fileName + ".txt");
+		assert stream != null;
+		return new Scanner(stream);
+
 	}
 }
--- src/main/java/org/example/dayOne.java
+++ src/main/java/org/example/dayOne.java
@@ -1,2 +1,32 @@
-package org.example;public class dayOne {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class dayOne {
+    public static void main(String[] args){
+
+        Scanner myScanner = GetScanner.get("Day-1");
+        ArrayList<Integer> calories = new ArrayList<>();
+
+        int current = 0;
+
+        while (myScanner.hasNext()){
+            String string = myScanner.nextLine();
+            if(string.equals("")){
+                calories.add(current);
+                current = 0;
+            } else {
+                current += Integer.parseInt(string);
+            }
+        }
+
+        calories.sort(Collections.reverseOrder());
+        int topThree = calories.get(0) + calories.get(1) + calories.get(2);
+
+        System.out.println("The greatest number of calories carried by a single elf: " + calories.get(0));
+        System.out.println("The number of calories carried by the top three elves: " + topThree);
+
+    }
 }
